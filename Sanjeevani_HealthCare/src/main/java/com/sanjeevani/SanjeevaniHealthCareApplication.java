45433139befaf5ed580ccfd939c8c8848b587329
Ajay <ajaypatra.bangalore@gmail.com>
2018-07-28 14:40:59 +0530

    Hey Guys My First commit

---
+++ Sanjeevani_HealthCare/src/main/java/com/sanjeevani/SanjeevaniHealthCareApplication.java
@@ -0,0 +1,13 @@
+
+package com.sanjeevani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjeevaniHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanjeevaniHealthCareApplication.class, args);
+	}
+}
